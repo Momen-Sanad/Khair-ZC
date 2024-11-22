03134be45722d594246b7ec78bc44eefdf30e423
Abdullah Ayman <abdulluhayman@gmail.com>
2024-11-22 16:04:32 +0200

    Just for fixing conflicts

--- server/app.py
+++ server/app.py
@@ -5,23 +5,30 @@ from config import Config
 from models.dbSchema import db
 from apis.routes.auth_login import auth_bp  # Import the auth blueprint
 from apis.routes.create_charity import charity_bp
-from apis.routes.create_event import event_bp 
+from apis.routes.create_event import event_bp
+from authlib.integrations.flask_client import OAuth
+import oauthlib
+import oauth
 
 # Initialize the db object
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key' 
 
     # Load configuration
     app.config.from_object(Config)
 
     # Initialize SQLAlchemy with the app
     db.init_app(app)
+    oauth = OAuth(app)
+
 
     # Register the blueprints
     app.register_blueprint(auth_bp, url_prefix='/auth')  # Prefix routes with /auth
-    app.register_blueprint(auth_bp, url_prefix='/charity')  # Prefix routes with /auth
-    app.register_blueprint(auth_bp, url_prefix='/event')  # Prefix routes with /auth
+    app.register_blueprint(charity_bp, url_prefix='/charity')  # Prefix routes with /auth
+    app.register_blueprint(event_bp, url_prefix='/event')  # Prefix routes with /auth
     
+
     return app
 
 # Create the app instance
