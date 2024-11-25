811367a9dbc5b06e3c8b4c6adab552dda42bcda1
KhalidUa <Kh522004@GMAIL.COM>
2024-11-25 18:28:43 +0200

    media

--- client/src/components/Media.tsx
+++ client/src/components/Media.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { useState,useEffect } from 'react';
 import '../assets/stylesheets/Media.css';
 
+
 const Media = () => {
-  return <div>Media Component</div>;
+  const [images,setImages]=useState<string[]>([]);
+  useEffect(()=>{
+    const numImages=20;
+    const imagesPaths=[];
+    for(let i=0;i<numImages;i++){
+      imagesPaths.push(`/images/KhairGallery/image${i+1}.jpg`);
+    }
+    setImages(imagesPaths);
+  },[])
+  return (
+    <div className='Gallery'>
+      {images.map((src,i)=>(
+        <img
+          src={src}
+          alt={`Image ${i+1}`}
+          className='GalleryImage'
+        />
+      ))}
+    </div>
+
+  );
 };
 
 export default Media;
